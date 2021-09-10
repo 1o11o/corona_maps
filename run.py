c3efb71343d7e8097ea2ab1b9188bff97b665aaf
lreiter <lorenz.r@gmx.at>
2021-09-10 19:13:30 +0200

    change run to main

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from streamlit import bootstrap
 
-#real_script = 'main.py'
-real_script = 'map3.py'
+real_script = 'main.py'
+#real_script = 'map3.py'
 bootstrap.run(real_script, f'run.py {real_script}', [], {})
